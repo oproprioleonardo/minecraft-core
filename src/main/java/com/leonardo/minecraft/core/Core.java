43f44516654bbc65932b01d4850d6b163ee84e22
Leonardo da Silva <oproprioleonardo@gmail.com>
2023-05-31 21:20:35 -0300

    feat: default configuration

--- src/main/java/com/leonardo/minecraft/core/Core.java
+++ src/main/java/com/leonardo/minecraft/core/Core.java
@@ -3,6 +3,7 @@ package com.leonardo.minecraft.core;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.leonardo.minecraft.core.api.uuid.UUIDProvider;
+import com.leonardo.minecraft.core.config.Configuration;
 import com.leonardo.minecraft.core.internal.uuid.CachedOkHttpUUIDProvider;
 import fr.minuskube.inv.InventoryManager;
 import okhttp3.OkHttpClient;
@@ -14,6 +15,7 @@ public class Core extends JavaPlugin {
     private final OkHttpClient client = new OkHttpClient.Builder().retryOnConnectionFailure(true).build();
     private UUIDProvider uuidProvider;
     private InventoryManager inventoryManager;
+    private Configuration defaultConfiguration;
 
     public Gson getGson() {
         return this.gson;
@@ -27,14 +29,20 @@ public class Core extends JavaPlugin {
         return this.uuidProvider;
     }
 
+    public Configuration getDefaultConfiguration() {
+        return defaultConfiguration;
+    }
+
     public OkHttpClient getClient() {
         return this.client;
     }
+
 
     @Override
     public void onEnable() {
         this.uuidProvider = new CachedOkHttpUUIDProvider(this);
         this.inventoryManager = new InventoryManager(this);
         this.inventoryManager.init();
+        this.defaultConfiguration = new Configuration(this, "config");
     }
 }
